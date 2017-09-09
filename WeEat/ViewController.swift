62335c5a7c759876b19fbe2d9ad4095950178550
Nova Guo <qiaochug@seas.upenn.edu>
2017-09-09 15:08:27 -0400

    report and request button

--- WeEat/ViewController.swift
+++ WeEat/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func requestConfirmed(){
+        print("Hello");
+    }
+    @IBAction func reportButton(_ sender: Any) {
+        let alert = UIAlertController(title: "Report", message: "Why do you want to report?", preferredStyle: UIAlertControllerStyle.alert)
 
+        alert.addTextField { (textField) in
+            textField.text = "Please enter here"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Submit", style: .default, handler: { [weak alert] (_) in
+            let textField = alert!.textFields![0]
+            print("Text field: \(textField.text)")
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler:nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    @IBAction func requestButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "Request Confirmation", message:
+            "Are you sure you want to send the request?", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alertController.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.default,handler: {UIAlertAction in self.requestConfirmed()}))
+        
+        alertController.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.default,handler: nil))
+    
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
